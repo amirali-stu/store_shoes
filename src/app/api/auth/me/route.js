e855303d59053227b6310ca446fa0e9999f06d76
amirAlimosolo <amirali.mosolo0@gmail.com>
2025-12-03 13:14:53 +0330

    Creating api me to get logged in user information

--- src/app/api/auth/me/route.js
+++ src/app/api/auth/me/route.js
@@ -1,7 +1,43 @@
-export function GET () {
-    try {
-        
-    } catch (error) {
-        
+import { cookies } from "next/headers";
+import { verifyToken } from "../../../../../utils/auth";
+import connectToDb from "../../../../../database/db";
+import UserModel from "../../../../../schema/user";
+
+export async function GET() {
+  try {
+    await connectToDb();
+    const cookieStore = await cookies();
+    const tokenCookie = cookieStore.get("token");
+    const token = tokenCookie?.value;
+    let findUserByEmailValue = null;
+
+    if (!token) {
+      return Response.json(
+        { code: "USER_IS_NOT_REGISTER", message: "کاربر ثبت نام نکرده است" },
+        { status: 401 }
+      );
+    } else {
+      const isTrustToken = verifyToken(token);
+      if (!isTrustToken) {
+        return Response.json(
+          { code: "TOKEN_HAS_EXPIRED.", message: "توکن نامعتبر یا منقض است" },
+          { status: 401 }
+        );
+      }
+      findUserByEmailValue = isTrustToken.email;
     }
+
+    const user = await UserModel.findOne(
+      { email: findUserByEmailValue },
+      { _id: 0, __v: 0, password: 0 }
+    );
+
+    return Response.json({ message: "success", data: user }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return Response.json(
+      { message: "Internal Server Error 500" },
+      { status: 500 }
+    );
+  }
 }
